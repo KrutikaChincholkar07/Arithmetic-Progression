4f80dc7246f43cb80a879b8d00cc85fac94cadbb
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-03-18 16:10:23 +0530

    Create GeneralForm.cpp
    An Arithmetic Progression (AP) is a sequence of numbers where the difference between consecutive terms is constant.
    General Form of AP
    a, a + d, a + 2d, a + 3d, ...
    Where:
    a → First term
    d → Common difference
    n → Number of terms
    
    If:
    First term a = 3
    Common difference d = 2
    Number of terms n = 5
    Sequence will be:
    3, 5, 7, 9, 11

---
+++ GeneralForm.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, d, n;
+
+    cout << "Enter first term: ";
+    cin >> a;
+
+    cout << "Enter common difference: ";
+    cin >> d;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    cout << "Arithmetic Progression: ";
+
+    for(int i = 0; i < n; i++) {
+        cout << a + i * d << " ";
+    }
+
+    return 0;
+}
