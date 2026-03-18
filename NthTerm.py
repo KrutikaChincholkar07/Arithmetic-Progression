db9d9b094fec9195e47348fd9c5d934c6b5a308d
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-03-19 00:39:52 +0530

    Create NthTerm.py
    An Arithmetic Progression (AP) is a sequence where the difference between consecutive terms remainsNth Term Formula
    a_n = a + (n - 1) × d
    nth_term = a + (n - 1) * d
    Where:
    aₙ → nth term of the AP
    a → First term
    d → Common difference
    n → Position of the term constant.
    Suppose:
    a = 3
    d = 2
    n = 5
    Calculation:
    a₅ = 3 + (5 - 1) × 2
    a₅ = 3 + 8
    a₅ = 11
    So the 5th term = 11.

---
+++ NthTerm.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first term: "))
+d = int(input("Enter common difference: "))
+n = int(input("Enter term number (n): "))
+
+nth = a + (n - 1) * d
+
+print("The", n, "th term of the AP is:", nth)
